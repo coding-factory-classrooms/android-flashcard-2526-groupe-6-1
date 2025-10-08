59eb19c799bd678c37d9c36963ce3f1ac6953bf7
Georges-Lucas <georgeslucas.grondin@gmail.com>
2025-10-08 10:35:06 +0200

    Ajout cotès java

--- app/src/main/java/com/example/flashcard/ListLevelActivity.java
+++ app/src/main/java/com/example/flashcard/ListLevelActivity.java
@@ -8,18 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LevelActivity extends AppCompatActivity {
+public class ListLevelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_level);
+        setContentView(R.layout.activity_list_level);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.flashcard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    public Button startbutton, aproposbutton, selectbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Log.d(TAG, "Hello Flashcard");
+        startbutton = findViewById(R.id.startbutton);
+        startbutton.setOnClickListener(view ->{
+             Intent intent = new Intent(this, LevelActivity.class);
+
+
+             startActivity(intent);
+         });
+
+        aproposbutton = findViewById(R.id.aproposbutton);
+        aproposbutton.setOnClickListener(view ->{
+            Intent intent = new Intent(this, AboutActivity.class);
+
+            startActivity(intent);
+        });
+
+        selectbutton= findViewById(R.id.selectbutton);
+        selectbutton.setOnClickListener(view ->{
+            Intent intent = new Intent(this, ListLevelActivity.class);
+
+            startActivity(intent);
+        });
     }
 }
