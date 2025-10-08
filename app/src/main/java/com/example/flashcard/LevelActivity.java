356dc6f5fc407af7e9250a4109911296cf2f5576
Georges-Lucas <georgeslucas.grondin@gmail.com>
2025-10-08 14:27:43 +0200

    gestion des question et des reponses

--- app/src/main/java/com/example/flashcard/LevelActivity.java
+++ app/src/main/java/com/example/flashcard/LevelActivity.java
@@ -2,6 +2,8 @@ package com.example.flashcard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class LevelActivity extends AppCompatActivity {
+
+    public TextView questiontextView;
+    public String question;
+    public  int choix;
+    public RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,22 @@ public class LevelActivity extends AppCompatActivity {
             return insets;
         });
         Intent srcIntent = getIntent();
+
+        //modification du nom de la question
+        questiontextView = findViewById(R.id.questiontextView);
+        question = srcIntent.getStringExtra("question_text");
+        questiontextView.setText(question);
+
+        //modification des reponses
+        List<String> reponse = srcIntent.getStringArrayListExtra("reponse");
+
+            radioButton = findViewById(R.id.responseradioButton);
+            radioButton.setText(reponse.get(0));
+            radioButton = findViewById(R.id.responseradioButton2);
+            radioButton.setText(reponse.get(1));
+            radioButton = findViewById(R.id.responseradioButton3);
+            radioButton.setText(reponse.get(2));
+            radioButton = findViewById(R.id.responseradioButton4);
+            radioButton.setText(reponse.get(3));
     }
 }
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -17,6 +17,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
@@ -65,18 +68,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 // Selon l'index, lancer une activité différente
                 Intent intent;
+                String question = "Quelle est la capitale de la France ?";
+                ArrayList<String> answer = new ArrayList<>();
+                answer.add(0, "response 1");
+                answer.add(1, "response 2");
+                answer.add(2, "response 3");
+                answer.add(3, "response 4");
                 switch (which) {
                     case 0: // Facile
                         intent = new Intent(MainActivity.this, LevelActivity.class);
                         intent.putExtra("difficulty", 3);
+                        intent.putExtra("question_text", question);
+                        intent.putExtra("reponse", answer);
                         break;
                     case 1: // Moyen
                         intent = new Intent(MainActivity.this, LevelActivity.class);
                         intent.putExtra("difficulty", 5);
+                        intent.putExtra("question_text", question);
+                        intent.putExtra("reponse", answer);
                         break;
                     case 2: // Difficile
                         intent = new Intent(MainActivity.this, LevelActivity.class);
                         intent.putExtra("difficulty", 10);
+                        intent.putExtra("question_text", question);
+                        intent.putExtra("reponse", answer);
                         break;
                     default:
                         return; // ne rien faire si problème
