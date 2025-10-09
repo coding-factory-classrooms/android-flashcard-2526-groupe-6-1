08a4b8bce48812ce2c510a183b9562a0ab3fe79c
dimensi4 <lmarnette0@gmail.com>
2025-10-09 15:40:33 +0200

    add gjon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.code.gson:gson:2.13.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.code.gson:gson:2.13.2")
 }
